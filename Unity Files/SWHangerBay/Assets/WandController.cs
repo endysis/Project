d8cf486c331021b08e6b8ac218be30466f9aa742
Henry Wallis <henrywallis1996@gmail.com>
2017-01-15 11:46:40 +0000

    hanger

---
+++ Unity Files/SWHangerBay/Assets/WandController.cs
@@ -0,0 +1,70 @@
+﻿
+﻿using UnityEngine;
+using System.Collections;
+
+public class WandController : MonoBehaviour {
+	
+	private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+	public bool gripButtonDown = false;
+	public bool gripButtonUp = false;
+	public bool gripButtonPressed = false;
+	
+	
+	
+	private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+	public bool triggerButtonDown = false;
+	public bool triggerButtonUp = false;
+	public bool triggerButtonPressed = false;
+	
+	
+	
+
+	
+	
+	
+	private SteamVR_TrackedObject trackedObj;
+	private SteamVR_Controller.Device controller {
+		get {
+			return SteamVR_Controller.Input((int) trackedObj.index);
+		}
+	}
+	
+	
+	// Use this for initialization
+	void Start () {
+		trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (controller == null) {
+			Debug.Log("Controller not installed");
+			return;
+		}
+		
+		
+		gripButtonDown = controller.GetPressDown(gripButton);
+		gripButtonUp = controller.GetPressUp(gripButton);
+		gripButtonPressed = controller.GetPressUp(gripButton);
+		
+		triggerButtonDown = controller.GetPressDown(triggerButton);
+		triggerButtonUp = controller.GetPressDown(triggerButton);
+		triggerButtonPressed = controller.GetPressDown(triggerButton);
+		
+		if (gripButtonDown) {
+			Debug.Log("Grip Button Pressed");
+		}
+		if (gripButtonUp) {
+			Debug.Log("Grip BUtton Unpressed");
+		}
+		if (triggerButtonDown)
+		{
+			Debug.Log("Trigger Button Pressed");
+		}
+		if (triggerButtonUp)
+		{
+			Debug.Log("Trigger Button Unpressed");
+		}
+		
+	}
+}
---
+++ Unity Files/SWHangerBay/Assets/WeaponChange.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponChange : MonoBehaviour {
+
+
+	private GameObject l1;
+	private GameObject l2;
+
+	//public GameObject animation;
+
+	private WeaponSwitching otherScript;
+
+
+	public int weaponNumber;
+
+
+	// Use this for initialization
+	void Start () {
+		l1 = GameObject.Find("L1");
+		l2 = GameObject.Find ("L2");
+
+		l1.SetActive(false);
+		l2.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	
+		otherScript = GetComponent<WeaponSwitching>();
+		weaponNumber = otherScript.currentWeapon;
+
+
+
+		if (weaponNumber == 0) {
+			l1.SetActive(false);
+			l2.SetActive(false);
+		}
+
+		if (weaponNumber == 1) {
+			l1.SetActive(true);
+			l2.SetActive(false);
+		}
+
+		if (weaponNumber == 2) {
+			l2.SetActive(true);
+			l1.SetActive(false);
+		}
+
+
+	}
+}
