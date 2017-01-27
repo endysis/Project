2373a01d0944081ee6f397a0cd33e0b6835606ff
endysis <shea_o@me.com>
2017-01-27 16:16:01 +0000

    Added Second Distraction

--- Unity Files/SWHangerBay/Assets/Scripts/ColliderRotate.cs
+++ Unity Files/SWHangerBay/Assets/Scripts/ColliderRotate.cs
@@ -5,22 +5,34 @@ public class ColliderRotate : MonoBehaviour {
 
 	public GameObject world;
     public GameObject objectPoint;
-    public GameObject distractObject;
-	public bool left = false;
+    public GameObject attentionObject;
+    public GameObject[] distractObject;
+
+   // public AudioSource attentionAudio;
+   // public AudioSource distractAudio;
+
+    public bool left = false;
 	public bool right = false;
 	public float rotationAmount;
 	public float rotationSpeed;
-    float timer = 4.0f;
-    public bool timerOn = false;
-    private Animator anim;
+    public float timer = 4.0f;
+    private bool timerOn = false;
+    private Animator animAt;
+    private Animator[] animDis;
     private int collideCount = 0; // So the animation only can play once.
-
 
 
 	// Use this for initialization
 	void Start () {
-        anim = distractObject.GetComponent<Animator>();
-	}
+        animAt = attentionObject.GetComponent<Animator>();
+        animDis = new Animator[distractObject.Length];
+       
+        for (int i = 0; i < distractObject.Length; i++) {
+            animDis[i] = distractObject[i].GetComponent<Animator>();
+        }
+ 
+    
+      }
 	
 
 	// Update is called once per frame
@@ -37,25 +49,39 @@ public class ColliderRotate : MonoBehaviour {
         //Debug.Log("Rotation Amount : " + world.transform.rotation.y);
     }
 
+
     void OnTriggerEnter(Collider other)
     {
         Debug.Log("Collided");
+        //Debug.Log(distractObject.Length);
+        Debug.Log(animDis.Length);
 
-
-        anim.SetBool("posCheck",true);
-
-        if (collideCount <= 0)
+        if (collideCount <= 0) // So sequences only play if collided with once.
         {
+            // So Start Attention Sequence.
+            animAt.SetBool("posCheck", true);
+            //attentionAudio.Play();
+
             timerOn = true;
             collideCount++;
         }
+
         else {
             // Do nothing
         }
     }
 
 
+
     void rotateWorld() {
+
+        // Play the distract animation at this point
+        //distractAudio.Play();
+
+        for (int i = 0; i < animDis.Length; i++) {
+            animDis[i].SetBool("posCheck",true);
+        }
+
         if (collideCount == 1) { // So the world only rotates once.
             collideCount++;
             if ((left == false && right == false) || (left == true && right == true))
@@ -63,7 +89,7 @@ public class ColliderRotate : MonoBehaviour {
                 // Do nothing
                 Debug.Log("Impossible Input || No direction selected");
             }
-            else if (left == true)
+            else if (left == true)                       
             {
                 world.transform.RotateAround(objectPoint.transform.position, -world.transform.up * rotationAmount, Time.deltaTime * rotationSpeed);
             }
---
+++ Unity Files/SWHangerBay/Assets/Scripts/lightSaber.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightSaber : MonoBehaviour {
+
+
+    LineRenderer lineRend;
+    public Transform startPos;
+    public Transform endPos;
+
+
+	// Use this for initialization
+	void Start () {
+        lineRend = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lineRend.SetPosition(0, startPos.position);
+        lineRend.SetPosition(1, endPos.position);
+	}
+}
