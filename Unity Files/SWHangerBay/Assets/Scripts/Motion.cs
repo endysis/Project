41a27be8cf99753937e504016a550005303668bf
endysis <shea_o@me.com>
2017-02-02 20:48:42 +0000

    Add files via upload

---
+++ Unity Files/SWHangerBay/Assets/Scripts/Motion.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motion : MonoBehaviour {
+
+
+    public GameObject camera;
+    public GameObject model;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+   void OnTriggerEnter(){
+        camera.transform.parent = model.transform;
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
