58f0d3e99a509233f811cfd917413d0024aaf62b
endysis <shea_o@me.com>
2017-02-25 16:50:43 +0000

    Added rotation script

---
+++ Unity Files/SWHangerBay/Assets/Scripts/OrbitRotate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitRotate : MonoBehaviour {
+
+    public GameObject worldPosition;
+    public GameObject objectPlayerPosition;
+    private float lastLoggedRotPosition; // Last logged rotational position
+    private float currRotPosition; // Current rotational position
+
+
+	// Use this for initialization
+	void Start () {
+        lastLoggedRotPosition = objectPlayerPosition.transform.rotation.y;
+        currRotPosition = lastLoggedRotPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Current Rot Y position" + currRotPosition);
+	}
+
+    void rotateWorld() {
+
+    }
+
+}
